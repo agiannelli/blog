6b450ea1894bfac53095b6da5fd3145941428abe
Amanda Giannelli <giannelli.tech@gmail.com>
2021-07-22 13:58:28 -0400

    Init commit

---
+++ src/app.js
@@ -0,0 +1,105 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+// Define paths for express config
+const publicDir = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDir))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Amanda Giannelli'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Amanda Giannelli'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'Welcome to the help page...',
+        name: 'Amanda Giannelli'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    const address = req.query.address
+
+    if (!address) {
+        return res.send({
+            error: 'Location cannot be null!'
+        })
+    }
+
+    geocode(address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecast) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            return res.send({
+                address: address,
+                location,
+                forecast
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term!'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Help article not found.',
+        name: 'Amanda Giannelli'
+    })
+})
+
+// wildcard to redirect to 404 (this is why it must go last!)
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Page not found.',
+        name: 'Amanda Giannelli'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,41 @@
+const request = require('request')
+
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+const forecast = (lat, long, callback) => {
+    const url =
+        'http://api.weatherstack.com/current?access_key=9e7a75971846c588751d83cb7ecea012&query=' +
+        lat +
+        ',' +
+        long +
+        '&units=f'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            var weather = body.current
+            callback(
+                undefined,
+                weather.weather_descriptions[0] +
+                    '. It is currently ' +
+                    weather.temperature +
+                    ' degress out. It feels like ' +
+                    weather.feelslike +
+                    ' degrees out.'
+            )
+        }
+    })
+}
+
+module.exports = forecast
